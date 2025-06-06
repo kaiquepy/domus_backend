b40c461f63f1109cc7f893ba48883b5687581ed7
AndreLeite121 <andreribeiro139@gmail.com>
2025-06-05 21:30:39 -0300

    Update app.py

--- domus_backend/app.py
+++ domus_backend/app.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 
-from domus_backend.routers import auth
-from domus_backend.routers import admin  # Adicionado import do admin
+from domus_backend.routers import auth, users
+from domus_backend.database import engine, Base
 
+# Create the database tables
+Base.metadata.create_all(bind=engine)
+
+# Initialize the FastAPI application
 app = FastAPI()
 
 app.include_router(auth.router)
-app.include_router(admin.router)  # Inclui o roteador de admin
+app.include_router(users.router)
+
 
 @app.get('/')
 def read_root():
-    return {'Message': 'Olá Mundo!'}  # Corrigido para português conforme teste
+    return {'Message': 'Hello World'}
