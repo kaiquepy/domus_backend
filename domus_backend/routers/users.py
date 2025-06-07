14e06ef84f8a90ad42d286856df6b3b3f08287c2
AndreLeite121 <andreribeiro139@gmail.com>
2025-06-06 21:14:30 -0300

    crudUsers

--- domus_backend/routers/users.py
+++ domus_backend/routers/users.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from domus_backend.database import get_session
 from domus_backend.models import User
-from domus_backend.schemas import UserCreate, UserPublic
+from domus_backend.schemas import UserCreate, UserPublic, UserUpdate
 
 router = APIRouter(prefix='/users', tags=['users'])
 
@@ -33,3 +33,33 @@ def create_user(user: UserCreate, session: Session = Depends(get_session)):
     session.refresh(db_user)
     
     return db_user
+
+@router.put('/{user_id}', response_model=UserPublic)
+def update_user(
+    user_id: int, user_update: UserUpdate, session: Session = Depends(get_session)
+):
+    db_user = session.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+
+    for key, value in user_update.model_dump(exclude_unset=True).items():
+        setattr(db_user, key, value)
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+    return db_user
+
+
+@router.delete('/{user_id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+        )
+
+    session.delete(db_user)
+    session.commit()
